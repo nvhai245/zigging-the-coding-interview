c05cfceb9801d85cd84e8386a207cf44e3680c20
Hai Nguyen <nvhai245@gmail.com>
2025-05-13 20:16:16 +0700

    updated build.zig

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -28,91 +28,37 @@ pub fn build(b: *std.Build) void {
     //     .optimize = optimize,
     // });
 
-    // We will also create a module for our other entry point, 'main.zig'.
-    const q1_1 = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/1_arrays_and_string/1.1_is_unique.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // iterate over the source files in the directory
 
-    // // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // // file path. In this case, we set up `exe_mod` to import `lib_mod`.
-    // exe_mod.addImport("zigging_the_coding_interview_lib", lib_mod);
+    const cwd = std.fs.cwd();
+    const dir = try cwd.openDir("src/", .{ .iterate = true });
+    var it = dir.iterate();
+    while (try it.next()) |entry| {
+        if (entry.kind == .directory) {
+            const sub_dir = try cwd.openDir(std.mem.concat(b.allocator, u8, &.{ "src/", entry.name }) catch unreachable, .{ .iterate = true });
+            var sub_it = sub_dir.iterate();
+            while (try sub_it.next()) |sub_entry| {
+                if (sub_entry.kind == .file and std.mem.endsWith(u8, sub_entry.name, ".zig")) {
+                    const moduleName = std.mem.trimRight(u8, sub_entry.name, ".zig");
+                    const module = b.createModule(.{
+                        .root_source_file = b.path(std.mem.concat(b.allocator, u8, &.{ "src/", entry.name, "/", sub_entry.name }) catch unreachable),
+                        .target = target,
+                        .optimize = optimize,
+                    });
 
-    // // Now, we will create a static library based on the module we created above.
-    // // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // // for actually invoking the compiler.
-    // const lib = b.addLibrary(.{
-    //     .linkage = .static,
-    //     .name = "zigging_the_coding_interview",
-    //     .root_module = lib_mod,
-    // });
+                    const file_unit_tests = b.addTest(.{
+                        .name = std.mem.concat(b.allocator, u8, &.{ moduleName, "_unit_tests" }) catch unreachable,
+                        .root_module = module,
+                    });
 
-    // // This declares intent for the library to be installed into the standard
-    // // location when the user invokes the "install" step (the default step when
-    // // running `zig build`).
-    // b.installArtifact(lib);
+                    b.installArtifact(file_unit_tests);
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
-    const q1_1_exe = b.addExecutable(.{
-        .name = "1.1_is_unique",
-        .root_module = q1_1,
-    });
+                    const run_file_unit_tests = b.addRunArtifact(file_unit_tests);
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(q1_1_exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(q1_1_exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+                    const test_step = b.step(std.mem.concat(b.allocator, u8, &.{ "test-", sub_entry.name }) catch unreachable, "Run the unit tests");
+                    test_step.dependOn(&run_file_unit_tests.step);
+                }
+            }
+        }
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run1.1", "Run q1.1 solution");
-    run_step.dependOn(&run_cmd.step);
-
-    // // Creates a step for unit testing. This only builds the test executable
-    // // but does not run it.
-    // const q1_1_unit_tests = b.addTest(.{
-    //     .root_module = q1_1,
-    // });
-    //
-    // const run_lib_unit_tests = b.addRunArtifact(q1_1_unit_tests);
-
-    const q1_1_unit_tests = b.addTest(.{
-        .name = "1.1_is_unique_test",
-        .root_module = q1_1,
-    });
-
-    b.installArtifact(q1_1_unit_tests);
-
-    const run_q1_1_unit_tests = b.addRunArtifact(q1_1_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test1.1", "Run unit tests for q1.1");
-    test_step.dependOn(&run_q1_1_unit_tests.step);
 }
--- src/1_arrays_and_string/1.1_is_unique.zig
+++ src/1_arrays_and_string/1.1_is_unique.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
 
 // This is a simple example of a function that takes a list of items and returns
-// true if all items in the list are unique. This function uses a fixed size array
-// to keep track of which items have been seen. Since the string is Unicode, we
+// true if all items in the list are unique. Since the string is Unicode, we
 // need to use Utf8View to iterate over the string and check each codepoint.
-// This function uses a hash map to keep track of which items have been seen.
-pub fn isUniqueUnicode(list: []const u8) !bool {
+// This function uses a hash map to keep track of which items have been seen, therefore
+// requires a custom allocator.
+pub fn isUniqueUnicode(list: []const u8, allocator: std.mem.Allocator) !bool {
     var utf8 = try std.unicode.Utf8View.init(list);
     var iterator = utf8.iterator();
-    var seen = std.AutoHashMap(u21, void).init(std.testing.allocator);
+    var seen = std.AutoHashMap(u21, void).init(allocator);
     defer seen.deinit();
     while (iterator.nextCodepoint()) |codepoint| {
         if (seen.contains(codepoint)) {
@@ -23,7 +23,7 @@ pub fn isUniqueUnicode(list: []const u8) !bool {
 // true if all items in the list are unique. Instead of using a hash map, we
 // use a fixed size array to keep track of which items have been seen. Since the string
 // is ASCII, we can use a smaller size array.
-pub fn isUniqueASCIIWithoutHashMap(list: []const u8) bool {
+pub fn isUniqueASCII(list: []const u8) bool {
     var seen: [128]bool = [_]bool{false} ** 128;
     for (list) |item| {
         if (seen[item]) {
@@ -35,20 +35,20 @@ pub fn isUniqueASCIIWithoutHashMap(list: []const u8) bool {
 }
 
 test "ASCII" {
-    try std.testing.expect(isUniqueASCIIWithoutHashMap("numbers"));
-    try std.testing.expect(!isUniqueASCIIWithoutHashMap("hello world"));
+    try std.testing.expect(isUniqueASCII("numbers"));
+    try std.testing.expect(!isUniqueASCII("hello world"));
 }
 
 test "Chinese" {
-    try std.testing.expect(try isUniqueUnicode("你好，世界！"));
-    try std.testing.expect(!(try isUniqueUnicode("你好，世界！你好，世界！")));
+    try std.testing.expect(try isUniqueUnicode("你好，世界！", std.testing.allocator));
+    try std.testing.expect(!try isUniqueUnicode("你好，世界！你好，世界！", std.testing.allocator));
 }
 
 test "Japanese" {
-    try std.testing.expect(try isUniqueUnicode("こんにちは"));
-    try std.testing.expect(!(try isUniqueUnicode("こんにちはこんにちは")));
+    try std.testing.expect(try isUniqueUnicode("こんにちは", std.testing.allocator));
+    try std.testing.expect(!try isUniqueUnicode("こんにちはこんにちは", std.testing.allocator));
 }
 
 test "Invalid UTF-8" {
-    try std.testing.expectError(error.InvalidUtf8, isUniqueUnicode("\xed\xa0\x80"));
+    try std.testing.expectError(error.InvalidUtf8, isUniqueUnicode("\xed\xa0\x80", std.testing.allocator));
 }
